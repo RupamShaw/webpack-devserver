2e221e0cb72d84e185b9ebaaf2ad081f1b5d9128
Rupam <rupam.shaw@gmail.com>
2017-10-08 02:05:34 +1100

    scss css babel loader

--- src/script-1.js
+++ src/script-1.js
@@ -1,3 +1,5 @@
 var msg = require('./script-2');
 require('./css/introComponent.css');
+require('./css/introComponents.scss');
+
 alert(msg);
--- webpack.config.js
+++ webpack.config.js
@@ -23,10 +23,11 @@ module.exports = {
 
             },
             {
-              test    : /\.css$/,
+              test    : /\.scss$|.css$/,
               use     : [
                 "style-loader",
-                "css-loader"
+                "css-loader",
+                "sass-loader"
               ]
             }
         ] //loaders
